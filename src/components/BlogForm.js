c923b48a27ffb20b4c6a3d5987eb3c87e2d927e6
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-01 16:08:23 -0500

    adds handle submit

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,19 +1,43 @@
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-function BlogForm() {
+function BlogForm({ addblogPost }) {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const blogPost = {
+      title,
+      body,
+    };
+
+    addblogPost(title, body);
+    setTitle("");
+    setBody("");
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicTitle">
         <Form.Label>Blog Title</Form.Label>
-        <Form.Control type="text" placeholder="Enter title" />
+        <Form.Control
+          onChange={handleTitleChange}
+          value={title}
+          type="text"
+          placeholder="Enter title"
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicBody">
         <Form.Label>Blog Body</Form.Label>
-        <Form.Control type="text" placeholder="Enter body" />
+        <Form.Control
+          onChange={handleBodyChange}
+          value={body}
+          type="text"
+          placeholder="Enter body"
+        />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
