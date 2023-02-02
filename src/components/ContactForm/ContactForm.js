319ee4980e0146463e9312adfabb68d7da49f845
BrandeYarborough <brandeyarborough@gmail.com>
2023-02-01 19:38:35 -0500

    completes handle submit function and add contact

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Form } from "react-bootstrap";
+import { nanoid } from 'nanoid'
 
 const INITIAL_CONTACT = [
   {
+    id: 1,
     firstName: "Brande",
     lastName: "Yarborough",
     address: "100 University Ridge",
@@ -16,7 +18,29 @@ function ContactForm() {
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [contacts, setContacts] = useState(INITIAL_CONTACT);
-}
+
+  const addContact = (newContact) => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newContact = {
+      id: nanoid(),
+      firstName, 
+      lastName,
+      address,
+      phoneNumber,
+    };
+
+    addContact(newContact);
+    setFirstName("");
+    setLastName("");
+    setAddress("");
+    setPhoneNumber("");
+
+    
+  }
 
 return (
   <form onSubmit={handleSubmit}>
@@ -31,4 +55,6 @@ return (
   <button type="submit">Add</button>
   </form>
 )
+}
+
 export default ContactForm;
